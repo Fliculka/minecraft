a04f7824fa3420fd01a411e91a46ea36c5fb5985
anikah <zakanika.holubcova@zslivingston.cz>
2024-03-18 15:31:41 +0000

    add first attempts at coding a house
    add code for minecraft education agent
    to bulid a house

---
+++ algo_01.py
@@ -0,0 +1,43 @@
+#Bulidig house
+
+#bas
+agent.give("cobblestone", 64, 1)
+agent.teleport()
+agent.move("forward")
+agent.move("up")
+
+#cobblestone base
+
+for count in range(4):
+	agent.place("down")
+	agent.move("forward")
+
+agent.move("back")
+.turn("right")
+
+for count in range(3):
+    agent.move("forward")
+    agent.place("down")
+
+agent.turn("right")4):
+    agent.move("forward")
+    agent.place("down")
+
+agent.turn("right")
+agent.move("forward")
+agent.place("down")
+agent.move("forward")
+agent.move("forward")
+agent.place("down")
+
+agent.give("planks", 64, 1)
+
+agent.move("up")
+agent.move("up")
+agent.move("back")
+
+for count in range(2):
+    agent.move("forwards")
+    agent.place("down")
+
+
